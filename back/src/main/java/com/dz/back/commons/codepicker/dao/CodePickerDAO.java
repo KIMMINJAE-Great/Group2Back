fdad2a5a5ec2cced7b88dda4ae8a0323961eab8f
이건우 <73516275+mafia917@users.noreply.github.com>
2023-08-06 18:39:54 +0900

    codepickerDTO가 생겼음
    codepicker 수정 2023 08 06(일)

--- back/src/main/java/com/dz/back/commons/codepicker/dao/CodePickerDAO.java
+++ back/src/main/java/com/dz/back/commons/codepicker/dao/CodePickerDAO.java
@@ -6,6 +6,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.dz.back.acc.acc1000.acc1010.dto.ACC1010EmpDTO;
+import com.dz.back.acc.acc1000.acc1011.dto.DeptDTO;
 import com.dz.back.acc.acc1000.acc1013.dto.ComDTO;
 import com.dz.back.acd.acd1000.acd1010.dto.CarDTO;
 import com.dz.back.commons.codepicker.mapper.CodePickerMapper;
@@ -27,5 +29,17 @@ public class CodePickerDAO {
 	
 	public List<CarDTO> RegCarfindByName(String name) {
 		return sqlSession.getMapper(CodePickerMapper.class).RegCarfindByName(name);
+	}
+	public List<DeptDTO> DeptfindByName(String name) {
+		return sqlSession.getMapper(CodePickerMapper.class).DeptfindByName(name);
+	}
+	public List<ACC1010EmpDTO> EmpfindByName(String name) throws Exception {
+	    try {
+	    	System.out.println("여기서 문제발생?");
+	        return sqlSession.getMapper(CodePickerMapper.class).EmpfindByName(name);
+	    } catch (Exception e) {
+	        // Optionally, log the error here.
+	        throw new Exception("DB에 에러 발생함", e);
+	    }
 	}
 }
