54f7cba13ff098073111a175a4a6a7f41b2ccf75
KIM MINJAE <minjae2717@gmail.com>
2023-08-15 10:57:04 +0900

    [update]운행기록부
    운행일 기준 조회, 조회조건 required 적용,
    운행일자, 운행시간 변경 시 유효성 검사로 마지막 일자 기준 전 일자 입력시 입력 방지.
    동일 기준 수정은 시간만 변경 가능 그러나 중복되는 시간은 방지

--- back/src/main/java/com/dz/back/acd/acd1000/acd1010/controller/ACD1010Controller.java
+++ back/src/main/java/com/dz/back/acd/acd1000/acd1010/controller/ACD1010Controller.java
@@ -27,37 +27,37 @@ import com.dz.back.acd.acd1000.acd1010.serviceImpl.ACD1010ServiceImpl;
 @RequestMapping("/regcar")
 public class ACD1010Controller {
 
-	@Autowired
-	ACD1010ServiceImpl regcarService;
+   @Autowired
+   ACD1010ServiceImpl regcarService;
 
-	@GetMapping("/cardlist")
-	public ResponseEntity<List<CarDTO>> getCardCarList() {
-		List<CarDTO> carCardList = regcarService.getCardCarList();
-		System.out.println(carCardList);
-		return ResponseEntity.ok(carCardList);
-	}
-	
-	
-	@GetMapping("/carsearch")
+   @GetMapping("/cardlist")
+   public ResponseEntity<List<CarDTO>> getCardCarList() {
+      List<CarDTO> carCardList = regcarService.getCardCarList();
+      System.out.println(carCardList);
+      return ResponseEntity.ok(carCardList);
+   }
+   
+   
+   @GetMapping("/carsearch")
     public ResponseEntity<List<CarDTO>> carSearch(@RequestParam(required = false) String lease_yn) {
 
         List<CarDTO> carSearchList;
         if (lease_yn == "" || lease_yn.equals("all")) {
             carSearchList = regcarService.getCardCarList();
         } else {
-        	if(lease_yn != null && lease_yn.isEmpty()) {
-        		lease_yn = null;
-        	}
+           if(lease_yn != null && lease_yn.isEmpty()) {
+              lease_yn = null;
+           }
 
             if (lease_yn != null) {
                 if (lease_yn.isEmpty()) {
-                	lease_yn = null;
+                   lease_yn = null;
                 } else if (lease_yn.equals("owned")) {
-                	lease_yn = "1";
+                   lease_yn = "1";
                 }else if (lease_yn.equals("rented")) {
-                	lease_yn = "2";
+                   lease_yn = "2";
                 }else if (lease_yn.equals("leased")) {
-                	lease_yn = "3";
+                   lease_yn = "3";
             }}
 
             CarDTO carSearch = new CarDTO();
@@ -67,80 +67,99 @@ public class ACD1010Controller {
         return ResponseEntity.ok().body(carSearchList);
     }
 
-//	Ä«ï¿½ï¿½ Å¬ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
-	@PostMapping("/getRegcarCard")
-	public ResponseEntity<CarDTO> getEmpCard(@RequestBody Map<String, String> car_cd) {
-		CarDTO dto = regcarService.getRegcarCard(car_cd.get("car_cd"));
-		return ResponseEntity.ok().body(dto);
+//   Ä«ï¿½ï¿½ Å¬ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+   @PostMapping("/getRegcarCard")
+   public ResponseEntity<CarDTO> getEmpCard(@RequestBody Map<String, String> car_cd) {
+      CarDTO dto = regcarService.getRegcarCard(car_cd.get("car_cd"));
+      return ResponseEntity.ok().body(dto);
 
-	}
-	
-	@PostMapping("/getCarsInfo")
-	public ResponseEntity<String> getCarsInfo() {
-	    List<CarDTO> carCardList = regcarService.getCardCarList();
+   }
+   
+   @PostMapping("/getCarsInfo")
+   public ResponseEntity<String> getCarsInfo() {
+       List<CarDTO> carCardList = regcarService.getCardCarList();
 
-	    // ì´ì „ì—ëŠ” carCardListë¥¼ CarDTOë¡œ ìºìŠ¤íŒ…í•˜ë ¤ê³  í–ˆìœ¼ë‚˜, ì´ì œëŠ” ë¦¬ìŠ¤íŠ¸ì˜ ìš”ì†Œë¥¼ ê°€ì ¸ì™€ì•¼ í•©ë‹ˆë‹¤.
-	    if (!carCardList.isEmpty()) {
-	        CarDTO carDTO = carCardList.get(0);
+       //  ´  „ — Š” carCardListë¥  CarDTOë¡  ×çìŠ¤ Œ… •˜  ¤ê³   –ˆ œ¼ ‚˜,  ´  œ Š” ë¦¬ìŠ¤ Š¸ ˜  š” †Œë¥  ê°   ¸   •¼  •© ‹ˆ ‹¤.
+       if (!carCardList.isEmpty()) {
+           CarDTO carDTO = carCardList.get(0);
 
-	        String abizcarNBNM = carDTO.getCar_nb() + '.' + carDTO.getCar_nm();
+           String abizcarNBNM = carDTO.getCar_nb() + '.' + carDTO.getCar_nm();
 
-	        CarDTO responseDTO = new CarDTO();
-	        responseDTO.setAbizcarNBNM(abizcarNBNM);
-	        System.out.println("getCarsInfo ì‹¤í–‰ !!!!!!!!");
-	        System.out.println("responseDTO :" + responseDTO);
-	        System.out.println("abizcarNBNM :" + abizcarNBNM);
+           CarDTO responseDTO = new CarDTO();
+           responseDTO.setAbizcarNBNM(abizcarNBNM);
+           System.out.println("getCarsInfo  ‹¤ –‰ !!!!!!!!");
+           System.out.println("responseDTO :" + responseDTO);
+           System.out.println("abizcarNBNM :" + abizcarNBNM);
 
-	        return ResponseEntity.ok().body(abizcarNBNM);
-	    } else {
-	        // ë§Œì•½ carCardListê°€ ë¹„ì–´ìˆë‹¤ë©´ ì²˜ë¦¬í•  ë‚´ìš©ì„ ì—¬ê¸°ì— ì¶”ê°€í•˜ì„¸ìš”.
-	        return ResponseEntity.notFound().build();
-	    }
-	}
+           return ResponseEntity.ok().body(abizcarNBNM);
+       } else {
+           // ×ºŒì•½ carCardListê°  ë¹„ì–´ ˆ ‹¤×¼  Òú˜ë¦¬ •   ‚´ š© „  —¬ê¸°ì— ì¶”ê  •˜ „¸ š”.
+           return ResponseEntity.notFound().build();
+       }
+   }
 
 
-	// Ä«ï¿½ï¿½ ï¿½ß°ï¿½
-	@PostMapping("/addcar")
-	public ResponseEntity<CarDTO> addRegCar(@RequestBody CarDTO dto) {
+   // Ä«ï¿½ï¿½ ï¿½ß°ï¿½
+   @PostMapping("/addcar")
+   public ResponseEntity<CarDTO> addRegCar(@RequestBody CarDTO dto) {
+      
+      
+       // ¿øº»Çü½Ä¿¡¼­ Ã³À½ºÎÅÍ 10ÀÚ¸®±îÁö¸¸ Àß¶ó¼­ 
+       String GetDt = dto.getGet_dt().substring(0, 10);
+       String DisposalDt = dto.getDisposal_dt().substring(0, 10);
+       String LfrDt = dto.getLfr_dt().substring(0, 10);
+       String LtoDt = dto.getLto_dt().substring(0, 10);
+       String IfrDt = dto.getIfr_dt().substring(0, 10);
+       String ItoDt = dto.getIto_dt().substring(0, 10);
 
-		int result = regcarService.addRegCar(dto);
-		System.out.println(result);
-		if (result == 1) {
-			System.out.println(dto);
-			return ResponseEntity.ok(dto);
-		} else {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-		}
-	}
+       // ³¯Â¥¸¦ ÀÚ¸¥ °ªÀ¸·Î DTO ¼öÁ¤
+       dto.setGet_dt(GetDt);
+       dto.setDisposal_dt(DisposalDt);
+       dto.setLfr_dt(LfrDt);
+       dto.setLto_dt(LtoDt);
+       dto.setIfr_dt(IfrDt);
+       dto.setIto_dt(ItoDt);
+       
+      
 
-	// Ä«ï¿½ï¿½ ï¿½ß°ï¿½
-	@PutMapping("/updatecar")
-	public ResponseEntity<CarDTO> updateRegCar(@RequestBody CarDTO dto) {
+      int result = regcarService.addRegCar(dto);
+      System.out.println(result);
+      if (result == 1) {
+         System.out.println(dto);
+         return ResponseEntity.ok(dto);
+      } else {
+         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+      }
+   }
 
-		int result = regcarService.updateRegCar(dto);
-		System.out.println(result);
-		if (result == 1) {
-			System.out.println(dto);
-			return ResponseEntity.ok(dto);
-		} else {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-		}
-	}
-	
-	//Ä«ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
-		@DeleteMapping("/deletecar/{car_cd}")
-		public void deleteRegCar(@PathVariable String car_cd) {
-			regcarService.deleteRegCar(car_cd);
-		}
-		
-		
-		//Ã¼Å©¹Ú½º°¡ ¼±ÅÃµÇ¾úÀ»¶§ ÇÑ²¨¹ø¿¡ »èÁ¦ÇÏ±â
-		 @DeleteMapping("/deletecar")
-		    public void deleteCheckedCar(@RequestBody List<CarDTO> CarList) {
-		        for (CarDTO car : CarList) {
+   // Ä«ï¿½ï¿½ ï¿½ß°ï¿½
+   @PutMapping("/updatecar")
+   public ResponseEntity<CarDTO> updateRegCar(@RequestBody CarDTO dto) {
 
-		        	regcarService.deleteRegCar(car.getCar_cd());
-		        }
-		    }
+      int result = regcarService.updateRegCar(dto);
+      System.out.println(result);
+      if (result == 1) {
+         System.out.println(dto);
+         return ResponseEntity.ok(dto);
+      } else {
+         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+      }
+   }
+   
+   //Ä«ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
+      @DeleteMapping("/deletecar/{car_cd}")
+      public void deleteRegCar(@PathVariable String car_cd) {
+         regcarService.deleteRegCar(car_cd);
+      }
+      
+      
+      //Ã¼Å©¹Ú½º°¡ ¼±ÅÃµÇ¾úÀ»¶§ ÇÑ²¨¹ø¿¡ »èÁ¦ÇÏ±â
+       @DeleteMapping("/deletecar")
+          public void deleteCheckedCar(@RequestBody List<CarDTO> CarList) {
+              for (CarDTO car : CarList) {
+
+                 regcarService.deleteRegCar(car.getCar_cd());
+              }
+          }
 
 }
--- back/src/main/java/com/dz/back/ace/ace1000/ace1010/serviceimpl/ACE1010Serviceimpl.java
+++ back/src/main/java/com/dz/back/ace/ace1000/ace1010/serviceimpl/ACE1010Serviceimpl.java
@@ -1,8 +1,12 @@
 package com.dz.back.ace.ace1000.ace1010.serviceimpl;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 import com.dz.back.ace.ace1000.ace1010.dao.ACE1010DAO;
@@ -49,14 +53,118 @@ public class ACE1010Serviceimpl implements ACE1010Service {
 		return dao.findMaxSeqNb(car_cd);
 	}
 	@Override
-	public List<AbizCarPersonDTO> findallbycar(String car_cd) {
+	public List<AbizCarPersonDTO> findallbycar(String car_cd,String startDate, String endDate) {
+		System.out.println("기간들어간 운행기록부 조회");
+		System.out.println("car_cd : "+ car_cd);
 		// TODO Auto-generated method stub
-		return dao.findallbycar(car_cd);
+		return dao.findallbycar(car_cd, startDate, endDate);
 	}
+	@Override
+	public List<AbizCarPersonDTO> findallbycar(String car_cd) {
+		System.out.println("기간이 안들어간 운행기록부 조회");
+		// TODO Auto-generated method stub
+		return dao.findallbycarByCarCd(car_cd);
+	}
+	
 	@Override
 	public int updateAbizCarPerson(AbizCarPersonDTO dto) {
 		
 		return dao.updateAbizCarPerson(dto);
 	}
+	@Override
+	public String checkUseDtAndStartTime(AbizCarPersonDTO dto) {
+		
+//		시간 10자리로 맞추기
+		String insertUseDt = dto.getUse_dt();
+		
+		if (insertUseDt != null && insertUseDt.length() >= 10) {
+			 insertUseDt = insertUseDt.substring(0, 10);
+			 dto.setUse_dt(insertUseDt);
+		}
+		System.out.println("수정시 use_dt 형태 확인");
+		System.out.println(insertUseDt);
+		
+//		입력시 시간 확인 
+		List<AbizCarPersonDTO> dtoForMaxUsedto  = findallbycar(dto.getCar_cd());
+		
+		AbizCarPersonDTO checkFinalInsertUseDtAndStartTime = new AbizCarPersonDTO();
+		
+		String checkInsertUseDt ="";
+		
+//		제일 마지막 운행일자 가져오기
+		if(!dtoForMaxUsedto.isEmpty()) {
+			checkFinalInsertUseDtAndStartTime = dtoForMaxUsedto.get(dtoForMaxUsedto.size() -1);
+			checkInsertUseDt=checkFinalInsertUseDtAndStartTime.getUse_dt();
+		} 
+		
+		
+		if(dto.getSeq_nb() > 0) { //수정
+			
+			// seq_nb가 일치하는 객체 찾기
+	        Optional<AbizCarPersonDTO> matchedDtoOpt = dtoForMaxUsedto.stream()
+	            .filter(d -> Objects.equals(d.getSeq_nb(), dto.getSeq_nb()))
+	            .findFirst();
+	        
+	        if (matchedDtoOpt.isPresent()) {
+	            AbizCarPersonDTO matchedDto = matchedDtoOpt.get();
+	            
+	            // insertUseDt와 matchedDto의 use_dt를 비교
+	            if (Objects.equals(insertUseDt, matchedDto.getUse_dt())) {
+	                // 여기서 수정을 진행합니다.
+	                System.out.println("수정 시작");
+	                int updateTimeCheckResult = updateTimeCheck(dto);
+	                System.out.println(updateTimeCheckResult);
+	                if(updateTimeCheckResult > 0) {
+	                    return "same time exist at working row";
+	                } else {
+	                    return "ok";
+	                }
+	            } else if(LocalDate.parse(insertUseDt).isBefore(LocalDate.parse(checkInsertUseDt))){
+	                return "before data exist";
+	            }
+	        } 
+		} else { //신규 입력
+			
+
+			if(dtoForMaxUsedto == null ||dtoForMaxUsedto.isEmpty()) {
+				return "ok";
+			} else {
+				System.out.println("시간확인용 DTO");
+				System.out.println(checkFinalInsertUseDtAndStartTime.toString());
+				
+				
+				
+				
+				int insertStartTime;
+				int checkInsertStartTime;
+				
+				if (LocalDate.parse(insertUseDt).isBefore(LocalDate.parse(checkInsertUseDt))) {
+					return "before data exist";
+				}
+				
+				if(dto.getStart_time() != null &&checkFinalInsertUseDtAndStartTime.getStart_time() !=null) {
+					checkInsertStartTime = Integer.parseInt( checkFinalInsertUseDtAndStartTime.getStart_time());
+					insertStartTime =  Integer.parseInt(dto.getStart_time());
+					if(insertUseDt.equals(checkInsertUseDt)) {
+						if(insertStartTime <= checkInsertStartTime) {
+							return "same time data exist";
+						}
+					}
+				}
+			}
+			
+			
+			
+
+		}
+			
+		return "ok";
+	}
+	@Override
+	public int updateTimeCheck(AbizCarPersonDTO dto) {
+		
+		return dao.updateTimeCheck(dto);
+	}
+	
 
 }
